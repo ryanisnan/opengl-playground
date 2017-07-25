ba7cd5eec799a9b589d2879eb13b5244e14717e5
ryanisnan <ryanisnan@gmail.com>
2017-07-24 18:42:57 -0700

    houston we have a triangle

--- main.cpp
+++ main.cpp
@@ -4,44 +4,6 @@
 using namespace std;
 
 
-class Tile {
-    private:
-        float x;
-        float y;
-        float width;
-        float height;
-        float r;
-        float g;
-        float b;
-        GLfloat theta;
-    public:
-        void renderTile();   
-        Tile(float x, float y, float width, float height, float r, float g, float b) {
-            this->x = x;
-            this->y = y;
-            this->width = width;
-            this->height = height;
-            this->r = r;
-            this->g = g;
-            this->b = b;
-            fprintf(stdout, "All is good!");
-        }     
-};
-
-void Tile::renderTile() {
-    fprintf(stdout, "Tile::renderTile() called");
-    glBegin(GL_QUADS);
-    glColor3f(0.0f, 0.0f, 0.0f);
-    glVertex2f(this->x, this->y);
-    glColor3f(0.0f, 0.0f, 0.0f);
-    glVertex2f(this->x + this->width, this->y);
-    glColor3f(1.0f, 0.0f, 0.0f);
-    glVertex2f(this->x + this->width, this->y + this->height);
-    glColor3f(1.0f, 0.0f, 0.0f);    
-    glVertex2f(this->x, this->y + this->height);
-    glEnd();
-}
-
 void setupDisplay() {
     // Clear the screen and the depth buffer
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -49,17 +11,15 @@ void setupDisplay() {
     // TODO: Figure out what this line does exactly - saw it mentioned in a tutorial
     glLoadIdentity();
 
-    // Create 4 tiles
-    Tile *tile1 = new Tile(-1.0f, 0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f);
-    Tile *tile2 = new Tile(-0.5f, 0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f);
-    Tile *tile3 = new Tile(0.0f, 0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f);
-    Tile *tile4 = new Tile(0.5f, 0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f);
-
-    // Render the tiles
-    tile1->renderTile();
-    tile2->renderTile();
-    tile3->renderTile();
-    tile4->renderTile();
+    // Draw stuff
+    glBegin(GL_TRIANGLES);
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex3f(0.0f, 1.0f, 0.0f);
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glVertex3f(-1.0f, -1.0f, 0.0f);
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glVertex3f(1.0f, -1.0f, 0.0f);
+    glEnd();
     
     // Do the things
     glFlush();
